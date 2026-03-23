36146d658c110cea7d67d883b3601a3e874b6a70
Feiyu <yangyang0102030405yy@gmail.com>
2026-03-23 21:05:18 +0800

    history upload

--- web/HydrogenX/run.py
+++ web/HydrogenX/run.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9037, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=3031, debug=True, use_reloader=False)
